cb3f1e79d89e62d6bb3001e2f8a6cce8e9e133a3
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-11-14 12:57:20 +0200

    Displayed an article by slug using the article oop model

--- models/article.js
+++ models/article.js
@@ -4,9 +4,15 @@ class ArticleModel extends BaseSQLModel {
     constructor() {
         super('article');
     }
+    
     async findAll(){
         const articles = await super.findAll()
         return articles
     }
+
+    async findOne(slug){
+        const article = await super.findOne('slug', slug)
+        return article
+    } 
 }
 module.exports = ArticleModel;
